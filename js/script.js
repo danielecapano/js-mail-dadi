a1b5c842eedb2bdc41934d5c8340c9d775fce14b
danielecapano <daniele.capano@gmail.com>
2023-10-09 17:38:47 +0200

    FIX - js code

--- js/script.js
+++ js/script.js
@@ -10,26 +10,29 @@ const mailList = [
 
 const userName = prompt("Inserisci il tuo nome");
 const userMail = prompt("Inserisci la tua mail");
+let isMailFound = false;
 
 for (let i = 0; i < mailList.length; i++) {
   const validMail = mailList[i];
   if (userMail === validMail) {
-    console.log("Accesso valido, puoi giocare");
-
-    const userPoint = Math.floor(Math.random() * (6 - 1) + 1);
-    console.log(`Punteggio dadi ${userName}: ${userPoint}`);
-
-    const computerPoint = Math.floor(Math.random() * (6 - 1) + 1);
-    console.log(`Punteggio dadi PC: ${computerPoint}`);
-
-    if (userPoint > computerPoint) {
-      console.log(`Comlimenti ${userName}, hai vinto`);
-    } else if (userPoint < computerPoint) {
-      console.log(`Mi dispiace ${userName}, hai perso`);
-    } else {
-      console.log("La partita è finita in parità");
-    }
+    isMailFound = true;
   }
 }
 
-console.log("accesso non valido, inserire una mail diversa");
+if (isMailFound) {
+  const userPoint = Math.floor(Math.random() * (6 - 1) + 1);
+  console.log(`Punteggio dadi ${userName}: ${userPoint}`);
+
+  const computerPoint = Math.floor(Math.random() * (6 - 1) + 1);
+  console.log(`Punteggio dadi PC: ${computerPoint}`);
+
+  if (userPoint > computerPoint) {
+    console.log(`Comlimenti ${userName}, hai vinto`);
+  } else if (userPoint < computerPoint) {
+    console.log(`Mi dispiace ${userName}, hai perso`);
+  } else {
+    console.log("La partita è finita in parità");
+  }
+} else {
+  console.log("Accesso non consentito, inserire una mail valida");
+}
